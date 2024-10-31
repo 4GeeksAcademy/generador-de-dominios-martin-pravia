64569d10650474e523e650580acb6866b167e6b5
martopravia <marto.pravia@gmail.com>
2024-10-31 16:00:57 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let dominios = [];
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let z = 0; z < noun.length; z++) {
+        for (let j = 0; j < extension.length; j++) {
+          dominios.push(`${pronoun[i]}${adj[x]}${noun[z]}${extension[j]}`);
+        }
+      }
+    }
+    document.getElementById("domain").innerHTML = dominios.join(`<p>`);
+  }
 };
